5552d10156ceb1817485c084578a2ecb6598f86a
Joshua Cooperrider <196146192+joshuacooperrider@users.noreply.github.com>
2025-04-06 17:29:55 -1000

    SignUp page complete
    Signup page allows users to select from one of two roles: Artist or Collector
    
    Currently there is no redirection based on role but we could implement redirection to different landing pages in the future

--- src/lib/dbActions.ts
+++ src/lib/dbActions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { Stuff, Condition } from '@prisma/client';
+import { Stuff, Condition, Role } from '@prisma/client';
 import { hash } from 'bcrypt';
 import { redirect } from 'next/navigation';
 import { prisma } from './prisma';
@@ -67,13 +67,14 @@ export async function deleteStuff(id: number) {
  * Creates a new user in the database.
  * @param credentials, an object with the following properties: email, password.
  */
-export async function createUser(credentials: { email: string; password: string }) {
+export async function createUser(credentials: { email: string; password: string; role?: 'Artist' | 'Collector' }) {
   // console.log(`createUser data: ${JSON.stringify(credentials, null, 2)}`);
   const password = await hash(credentials.password, 10);
   await prisma.user.create({
     data: {
       email: credentials.email,
       password,
+      role: credentials.role as Role | undefined,
     },
   });
 }
