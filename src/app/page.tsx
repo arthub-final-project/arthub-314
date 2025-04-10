c4b3202bb3cb1d90eb6c99e5551ad5cb3bfea3b5
Justinrb998 <Justinrb998@gmail.com>
2025-04-09 23:37:39 -1000

    First Mockup
    Navbar needs more options like search bar and categories/tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,30 @@
 import { Col, Container, Image, Row } from 'react-bootstrap';
+import ArtworkCard from '@/components/ArtworkCard';
 
 /** The Home page. */
 const Home = () => (
   <main>
-    <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
-        </Col>
-
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Col>
+    <Container id="landing-page" fluid className="py-3" style={{ height: '100vh'}}>
+      <Row className="justify-content-center align-items-center ms-5">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <Col key={idx}>
+            <ArtworkCard />
+          </Col>
+        ))}
+      </Row>
+      <Row className="justify-content-center align-items-center ms-5 mt-4">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <Col key={idx}>
+            <ArtworkCard />
+          </Col>
+        ))}
+      </Row>
+      <Row className="justify-content-center align-items-center ms-5 mt-4">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <Col key={idx}>
+            <ArtworkCard />
+          </Col>
+        ))}
       </Row>
     </Container>
   </main>
