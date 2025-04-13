dcfc477dc47e7f521bde2333bc30478acef1b605
rolando-cadiz <143847732+rolando-cadiz@users.noreply.github.com>
2025-04-12 14:03:36 -1000

    Update validationSchemas.ts

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -1,1 +1,24 @@
-// import * as Yup from 'yup';
+import * as Yup from 'yup';
+
+export const AddGalleryItemSchema = Yup.object({
+  title: Yup.string().required(),
+  description: Yup.string().required(),
+  image: Yup.string().url().required(),
+});
+
+export type GalleryItem = {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  createdAt: Date;
+};
+
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  profileImage: string;
+  bio: string;
+  createdAt: Date;
+};
