253b388e3c26cad3cf6a01cf2b8253a021a960e7
Justinrb998 <165746301+Justinrb998@users.noreply.github.com>
2025-04-14 23:33:41 -1000

    Update page.tsx

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,77 @@
+import { getServerSession } from 'next-auth';
+import { Col, Container, Row, Table } from 'react-bootstrap';
+import { prisma } from '@/lib/prisma';
+import { adminProtectedPage } from '@/lib/page-protection';
+import authOptions from '@/lib/authOptions';
+
+const AdminPage = async () => {
+  const session = await getServerSession(authOptions);
+  adminProtectedPage(
+    session as {
+      user: { email: string; id: string; randomKey: string };
+    } | null,
+  );
+  const stuff = await prisma.stuff.findMany({});
+  const users = await prisma.user.findMany({});
+
+  return (
+    <main>
+      <Container id="list" fluid className="py-3">
+        <Row>
+          <Col>
+            <h1>List Stuff Admin</h1>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Quantity</th>
+                  <th>Condition</th>
+                  <th>Owner</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {stuff.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.name}</td>
+                    <td>{item.quantity}</td>
+                    <td>{item.condition}</td>
+                    <td>{item.owner}</td>
+                    <td>
+                      {/* Add any action buttons or links here */}
+                      <button>Edit</button>
+                      <button>Delete</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>List Users Admin</h1>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Email</th>
+                  <th>Role</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user) => (
+                  <tr key={user.id}>
+                    <td>{user.email}</td>
+                    <td>{user.role}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+    </main>
+  );
+};
+
+export default AdminPage;
