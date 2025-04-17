2fc089fd836e3e71083446444c1ca263e1696fdf
rolando-cadiz <143847732+rolando-cadiz@users.noreply.github.com>
2025-04-17 11:41:04 -1000

    w

--- src/components/ArtworkCard.tsx
+++ src/components/ArtworkCard.tsx
@@ -1,15 +1,16 @@
 'use client';
+
 import React from 'react';
-import { Card, Row, Col } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
 const ArtworkCard = () => (
   <Card bg={'dark'} border={"light"} style={{ width: '20rem', padding: '0', borderWidth: '2px' }}>
     <a href="#" style={{ display: 'block' }}>
       <Card.Img
-      variant="top"
-      src="/medieval.webp"
-      alt="Click me"
-      style={{ width: '100%', display: 'block' }} 
+        variant="top"
+        src="/medieval.webp"
+        alt="Click me"
+        style={{ width: '100%', display: 'block' }}
       />
     </a>
     <Card.Body style={{ color: 'white', textAlign: 'center'}}>
